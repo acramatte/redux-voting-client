960a6f2f4389b2844d630f993e1a5587f84e4d5f
acramatte <alexis.cramatte@comerge.net>
2016-02-18 22:32:53 +0100

    Added component to vote Added component showing winner

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Voting from './components/Voting';
+
+const pair = ['Transpotting', '28 Days Later'];
+
+ReactDOM.render(
+    <Voting pair={pair} hasVoted="Trainspotting"/>,
+    document.getElementById('app')
+);
